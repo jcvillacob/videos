b245464fb7a656744b4e4cd700ca3d165b01ecc0
Juan Camilo Villacob <jucaviza6@gmail.com>
2023-07-26 17:14:21 -0500

    videos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ReproductorComponent } from './components/reproductor/reproductor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    ReproductorComponent
   ],
   imports: [
     BrowserModule,
