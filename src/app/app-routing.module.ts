198e0b57365498f9ca6e06437d44309518123dc0
Juan Camilo Villacob <jucaviza6@gmail.com>
2023-08-09 13:00:32 -0500

    Todos los videos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
+import { ReproductorComponent } from './components/reproductor/reproductor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', pathMatch: 'full', redirectTo: 'home'},
+  { path:'home', component: TodosComponent},
+  { path:'video', component: ReproductorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ReproductorComponent } from './components/reproductor/reproductor.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ReproductorComponent
+    ReproductorComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
